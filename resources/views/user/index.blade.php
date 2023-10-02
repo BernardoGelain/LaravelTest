c5163362a39fb7dc6da6afd7938d6e36f15d6a55
Bernardo Gelain Dariva <bernardogdariva@gmail.com>
2023-10-02 16:53:50 -0300

    estilização

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,11 +6,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <title>Users</title>
 </head>
 
 <body>
-
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <div class="container">
         <h1>Lista de Usuários</h1>
 
